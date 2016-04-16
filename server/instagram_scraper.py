5fca176a1486b842ebeb089c9f925a5969de8582
maozillah <kayexmao@gmail.com>
2016-04-16 15:45:06 -0400

    error handling: null captions

--- server/instagram_scraper.py
+++ server/instagram_scraper.py
@@ -39,11 +39,14 @@ def igLocSearch(clickPos):
             locIDs[locID] = math.sqrt(math.pow(lat-float(currentLat),2) + math.pow(lng-float(currentLong),2))
 
         closestLocID = min(locIDs, key=locIDs.get)
+        print(closestLocID)
+
         getLocPhotos(closestLocID)
     else:
         print('no Instagram locations nearby')
         RESULTS['moment'].append({'error' :'no Instagram locations nearby'})
 
+    print(RESULTS)
     return RESULTS
 
 def getLocPhotos(LocID):
@@ -59,11 +62,14 @@ def getLocPhotos(LocID):
     if igPhotos['meta']['code'] == 200 and igPhotos['data']:
         
         totalImgs = len(igPhotos['data'])
+
         # for photo in igPhotos['data']:
         #     print(photo['id'])
 
         # select random photo
-        photo = igPhotos['data'][randint(0,totalImgs)]
+        photo = igPhotos['data'][randint(0,totalImgs-1)]
+
+        # photo = igPhotos['data'][1]
         # print("total images",totalImgs)
         # print("used images",numImgsUsed)
 
@@ -78,14 +84,25 @@ def getLocPhotos(LocID):
 
         #TODO: error handling if all images have been used
 
-        RESULTS['moment'].append({
-            'img_url' : photo['images']['standard_resolution']['url'],
-            'caption' : photo['caption']['text'],
-            'tags' : photo['tags'],
-            'url' : photo['link'],
-            'lat' : photo['location']['latitude'],
-            'long' : photo['location']['longitude']
-        })
+        if photo['caption']:
+            RESULTS['moment'].append({
+                'img_url' : photo['images']['standard_resolution']['url'],
+                'caption' : photo['caption']['text'],
+                'tags' : photo['tags'],
+                'url' : photo['link'],
+                'lat' : photo['location']['latitude'],
+                'long' : photo['location']['longitude']
+            })
+        else:
+            RESULTS['moment'].append({
+                'img_url' : photo['images']['standard_resolution']['url'],
+                'caption' : "no caption",
+                'tags' : photo['tags'],
+                'url' : photo['link'],
+                'lat' : photo['location']['latitude'],
+                'long' : photo['location']['longitude']
+            })
+
 
         # add new image id to text file
         # with open('usedImgs.txt', 'a') as file:
@@ -94,10 +111,9 @@ def getLocPhotos(LocID):
         print('no instagram images nearby')
         RESULTS['moment'].append({'error' :'no instagram images nearby'})
 
-
-# def main():
+def main():
     # test with sheridan college location
-    # igLocSearch("43.46834202822045,-79.69888508319855")
+    igLocSearch("43.468314775456854,-79.69923913478851")
 
     # search no locations nearby
     # igLocSearch("20.3034175184893,-178.2421875")
